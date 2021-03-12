4a8130d648d0082013f50255da098eb6a4a29d7d
MatinAbTerm3 <79203163+MatinAbTerm3@users.noreply.github.com>
2021-03-12 15:35:07 +0330

    Add files via upload

---
+++ includes/Product.php
@@ -0,0 +1,51 @@
+<?php
+
+if(! class_exists('Product'))
+{
+	class Product
+	{
+		static function coloumnsList( $vars , $sep = ', ' )
+		{
+			$columns = array_keys($vars);
+			return join($sep , $columns);
+		}
+		static function valuesList( $vars , $sep = "', '")
+		{
+			$values = array_values($vars);
+			return "'". join($sep , $values)."'";
+		}
+		static function add($params)
+		{
+			$columnsString = self::coloumnsList($params);
+			$valuesString = self::valuesList($params);
+			$tableName = get_class();
+			$sql = "INSERT INTO {$tableName}({$columnsString})
+					VALUES({$valuesString})";
+			$result = $GLOBALS['db'] -> execute($sql);
+			if($result)
+			{
+				echo alertTemplate('اطلاعات درج شده با موفقیت ارسال شد!' , 'success');
+			}
+		}
+		static function update()
+		{
+			
+		}
+		static function delete()
+		{
+			
+		}
+		static function find($where = 'TRUE' , $order = 'id DESC' , $count = 12 , $offset = 0)
+		{
+			$tableName = get_class();
+			$sql = "SELECT * FROM {$tableName}
+					WHERE {$where}
+					ORDER BY {$order}
+					LIMIT {$offset},{$count}";
+			$table = $GLOBALS['db'] -> execute($sql);
+			return $table;
+		}
+	}
+}
+
+?>
